65f0eea9940e799f312d61fd25a2e3938255ee0c
zerosilver <duddms0_0@naver.com>
2022-01-30 18:37:02 +0900

    회원 웹 기능

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,6 +33,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/"; // 회원가입이 끝나면 홈화면으로 보내버림
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberlist";
     }
 }
 
